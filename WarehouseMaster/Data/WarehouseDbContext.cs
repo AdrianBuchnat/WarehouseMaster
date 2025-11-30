25b742cd76a73d345080de66aad4a22bc966cba0
Adrian Buchnat <adrianbuchnat@gmail.com>
2025-11-30 10:17:30 +0100

    Add WarehouseDbContext, Category and Products

---
+++ WarehouseMaster/Data/WarehouseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseMaster.Models;
+
+namespace WarehouseMaster.Data
+{
+    public class WarehouseDbContext : DbContext
+    {
+        public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- WarehouseMaster/models/Product.cs
+++ WarehouseMaster/models/Product.cs
@@ -1,8 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WarehouseMaster.models
+namespace WarehouseMaster.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Sku { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
     }
 }
